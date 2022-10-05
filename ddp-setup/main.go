98265a1f23a02aef503fd1cc874cd48a285bd52e
Hendrik Ziegler <hendrikziegler@outlook.de>
2022-10-05 15:46:05 +0200

    recompiling the stdlib and runtime manually on windows, because the makefiles don't work

--- ddp-setup/main.go
+++ ddp-setup/main.go
@@ -35,16 +35,19 @@ func main() {
 		if err != nil {
 			WarnF("error getting absolute Path: %s", err)
 		}
+		gccCmd = filepath.ToSlash(gccCmd)
 		arCmd, err = filepath.Abs(filepath.Join("mingw64", "bin", "ar"))
 		if err != nil {
 			WarnF("error getting absolute Path: %s", err)
 		}
+		arCmd = filepath.ToSlash(arCmd)
 		makeCmd, err = filepath.Abs(filepath.Join("mingw64", "bin", "mingw32-make"))
 		if err != nil {
 			WarnF("error getting absolute Path: %s", err)
 		}
+		makeCmd = filepath.ToSlash(makeCmd)
 
-		InfoF("using unzipped mingw64 for gcc, ar and make")
+		DoneF("using unzipped mingw64 for gcc, ar and make")
 	} else if !hasGcc && runtime.GOOS != "windows" {
 		ErrorF("gcc not found, aborting")
 		os.Exit(1)
@@ -60,13 +63,13 @@ func main() {
 				ErrorF("mingw32-make not found, aborting")
 				os.Exit(1)
 			}
+			makeCmd = filepath.ToSlash(makeCmd)
 		}
 	}
 
 	if isSameGccVersion() {
-		InfoF("gcc versions match")
+		DoneF("gcc versions match")
 	} else {
-		InfoF("local gcc version, and kddp gcc version mismatch")
 		InfoF("re-building runtime and stdlib")
 		recompileLibs()
 	}
@@ -80,21 +83,32 @@ func isSameGccVersion() bool {
 	if err != nil {
 		return false
 	}
+	gccVersion = strings.Trim(gccVersion, "\n") // TODO: this
 	kddpVersionOutput, err := runCmd("", filepath.Join("bin", "kddp"), "version")
 	if err != nil {
 		return false
 	}
 	gccVersionLine := strings.Split(kddpVersionOutput, "\n")[2]
 	kddpGccVersion := strings.Split(gccVersionLine, " ")[2]
-	return gccVersion == kddpGccVersion
+	match := gccVersion == kddpGccVersion
+	if !match {
+		InfoF("local gcc version, and kddp gcc version mismatch (%s vs %s)", gccVersion, kddpGccVersion)
+	}
+	return match
 }
 
 func recompileLibs() {
-	if _, err := runCmd("lib/runtime", makeCmd, "build", fmt.Sprintf("CC=%s", gccCmd), fmt.Sprintf("AR=%s", arCmd)); err != nil {
-		return
-	}
-	if _, err := runCmd("lib/stdlib", makeCmd, "build", fmt.Sprintf("CC=%s", gccCmd), fmt.Sprintf("AR=%s", arCmd)); err != nil {
-		return
+	if runtime.GOOS == "linux" {
+		if _, err := runCmd("lib/runtime/", makeCmd); err != nil {
+			return
+		}
+		DoneF("re-compiled the runtime")
+		if _, err := runCmd("lib/stdlib/", makeCmd); err != nil {
+			return
+		}
+		DoneF("re-compiled the stdlib")
+	} else if runtime.GOOS == "windows" {
+		compileLibsWindows()
 	}
 
 	InfoF("removing pre-compiled runtime")
@@ -107,22 +121,84 @@ func recompileLibs() {
 	}
 
 	InfoF("copying re-compiled runtime")
-	if err := cp.Copy("lib/runtime/libddpruntime.a", "lib/"); err != nil {
+	if err := cp.Copy("lib/runtime/libddpruntime.a", "lib/libddpruntime.a"); err != nil {
 		ErrorF("error copying re-compiled runtime: %s", err)
 	}
 	InfoF("copying re-compiled stdlib")
-	if err := cp.Copy("lib/stdlib/libddpstdlib.a", "lib/"); err != nil {
+	if err := cp.Copy("lib/stdlib/libddpstdlib.a", "lib/libddpstdlib.a"); err != nil {
 		ErrorF("error copying re-compiled stdlib: %s", err)
 	}
 
 	InfoF("cleaning runtime directory")
+	if err := os.Remove("lib/runtime/libddpruntime.a"); err != nil {
+		WarnF("error while cleaning runtime directory: %s", err)
+	}
 	if err := removeObjects("lib/runtime/"); err != nil {
 		WarnF("error while cleaning runtime directory: %s", err)
 	}
 	InfoF("cleaning stdlib directory")
+	if err := os.Remove("lib/stdlib/libddpstdlib.a"); err != nil {
+		WarnF("error while cleaning stdlib directory: %s", err)
+	}
 	if err := removeObjects("lib/stdlib/"); err != nil {
 		WarnF("error while cleaning stdlib directory: %s", err)
 	}
+
+	DoneF("recompiled libraries")
+}
+
+func compileLibsWindows() {
+	getFiles := func(dir, ext string) []string {
+		files := make([]string, 0)
+
+		err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+			if d.IsDir() || err != nil {
+				return nil
+			}
+			if filepath.Ext(path) == ext {
+				files = append(files, strings.TrimPrefix(filepath.ToSlash(path), dir))
+			}
+			return nil
+		})
+
+		if err != nil {
+			WarnF("error while filtering files: %s", err)
+		}
+
+		return files
+	}
+
+	sources := getFiles("lib/runtime/", ".c")
+	objects := Map(sources, func(str string) string {
+		return filepath.Base(strings.ReplaceAll(str, ".c", ".o"))
+	})
+	args := append(make([]string, 0), "-c", "-Wall", "-Wno-format", "-O2", "-I./include/")
+	args = append(args, sources...)
+	if _, err := runCmd("lib/runtime", gccCmd, args...); err != nil {
+		return
+	}
+	args = append(make([]string, 0), "cr", "libddpruntime.a")
+	args = append(args, objects...)
+	if _, err := runCmd("lib/runtime", arCmd, args...); err != nil {
+		return
+	}
+	DoneF("re-compiled the runtime")
+
+	sources = getFiles("lib/stdlib/", ".c")
+	objects = Map(sources, func(str string) string {
+		return filepath.Base(strings.ReplaceAll(str, ".c", ".o"))
+	})
+	args = append(make([]string, 0), "-c", "-Wall", "-Wno-format", "-O2", "-I./include/", "-I../runtime/include/")
+	args = append(args, sources...)
+	if _, err := runCmd("lib/stdlib", gccCmd, args...); err != nil {
+		return
+	}
+	args = append(make([]string, 0), "cr", "libddpstdlib.a")
+	args = append(args, objects...)
+	if _, err := runCmd("lib/stdlib", arCmd, args...); err != nil {
+		return
+	}
+	DoneF("re-compiled the stdlib")
 }
 
 func runCmd(dir string, name string, args ...string) (string, error) {
@@ -140,7 +216,7 @@ func LookupCommand(cmd string) (string, bool) {
 	InfoF("Looking for %s", cmd)
 	path, err := exec.LookPath(cmd)
 	if err == nil {
-		InfoF("Found %s in %s", cmd, path)
+		DoneF("Found %s in %s", cmd, path)
 	} else {
 		WarnF("Unable to find %s", cmd)
 	}
@@ -161,4 +237,12 @@ func removeObjects(dir string) error {
 
 		return nil
 	})
+}
+
+func Map[T any](s []T, mapFunc func(t T) T) []T {
+	result := make([]T, 0, len(s))
+	for _, v := range s {
+		result = append(result, mapFunc(v))
+	}
+	return result
 }
